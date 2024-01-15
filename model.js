ab7045d72c7015f8a832f8e24759150e8f3917dc
Mohd-Akbar1 <34mohdakbar@gmail.com>
2024-01-15 15:30:26 +0530

    mongoose

--- model.js
+++ model.js
@@ -1,9 +1,9 @@
 const mongoose=require('mongoose')
 
 const UserSchema=new mongoose.Schema({
-    name:{type:String,required:1},
-    email:{type:String,required:1},
-    password:{type:String,required:1},
+    name:{type:String,required:true},
+    email:{type:String,required:true},
+    password:{type:String,required:true},
 },{timestamps:true})
 
 const User=new mongoose.model('User',UserSchema)
